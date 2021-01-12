d83bd8b878aa77e4320baf9cd636fce12056c553
j0rbe3n <jero.guzman@gmail.com>
2021-01-12 12:12:24 -0700

    Actualizacion videollamada

--- videollamada/urls.py
+++ videollamada/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
-from agora.views import Agora
+from .views import AgoraVideoCall
 from . import views
 
 app_name = 'videollamada'
 
 urlpatterns = [
     path('', views.videollamada, name="videollamada"),
-    path('success/', views.success, name="success"),
-    path('medico1/', Agora.as_view(app_id='8c9b9f4dde4c46d998166eceb384467a', channel='test'), name='medico1')
+    path('medico1/', AgoraVideoCall.as_view(app_id='8c9b9f4dde4c46d998166eceb384467a', channel='test'), name='medico1')
 
 ]
 
