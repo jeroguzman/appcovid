7df180f5a6ef0c698619cc12803c5ef56338dd20
j0rbe3n <jero.guzman@gmail.com>
2021-01-12 21:08:09 -0700

    update files

--- appcovid/settings/base.py
+++ appcovid/settings/base.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).parents[2]
--- appcovid/settings/prod.py
+++ appcovid/settings/prod.py
@@ -22,9 +22,9 @@ DATABASES = {
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#]
 
