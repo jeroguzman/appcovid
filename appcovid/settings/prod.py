b2220e04bdb3aa684ac7e2d0d5908cb5f7f42515
j0rbe3n <jero.guzman@gmail.com>
2021-01-12 21:14:47 -0700

    Update prod.py

--- appcovid/settings/prod.py
+++ appcovid/settings/prod.py
@@ -22,7 +22,7 @@ DATABASES = {
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = 'static/'
 
 #STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
