05068bff7e33f60b7b645db2a8f367520efa7800
Jero Guzman <jeroguzman@gmail.com>
2021-01-19 12:44:35 -0700

    Home listo, notificaciones casi listas

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-
+from .views import send_push, push_home
 app_name = "home_app"
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('send-push/', send_push, name='send-push'),
+    path('push-home/', push_home, name='push-home'),
+
 ]
