ba5a96e51abe037b25c3adeda87ab496e28f5f0b
Jeroben Guzmán <jeroguzman@gmail.com>
2021-01-21 16:10:57 -0700

    cambio urls

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
     path('send-push/', send_push, name='send-push'),
     path('push-home/', push_home, name='push-home'),
+    path('.well-known/pki-validation/ED2FF1C875C44F9BAC2E2B41264C40F0.txt', well, name='well'),
 
 
 ]
