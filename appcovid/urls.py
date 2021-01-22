54689800db9f78431b393c0f01ecf7fe20e95200
Jeroben Guzmán <jeroguzman@gmail.com>
2021-01-22 15:34:23 -0700

    Update Notif

--- appcovid/urls.py
+++ appcovid/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('videollamada/', include('applications.videollamada.urls')),
     re_path('', include('applications.home.urls')),
     re_path('', include('applications.users.urls')),
     path('', include('pwa.urls')),
+    
 ]
